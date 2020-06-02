d90cb182396b3fe360657f345816f9a5765d4f22
YashIsDeep <yashparth.gupta@gmail.com>
2020-06-02 17:50:14 +0530

    Added pre-commit hook code

---
+++ pre-commit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# A pre-commit hook that will stash any changes so that we are running our tests
+# against what we are committing, not what was on in memory.
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+status=0
+
+function regex_test() {
+	pattern="$1"
+	git diff-index --cached --name-only HEAD > 'temporaryInput.list'
+	while IFS= read -r line
+	do
+		FILE="$line"
+		if [[ $(grep -E $pattern $FILE) ]]; then
+	    	echo -e "${RED}Errors${NC} found in $FILE: Contains occurences of $pattern"
+            status=1
+		fi
+	done < "temporaryInput.list"
+    rm temporaryInput.list
+}
+
+git stash --quiet --keep-index
+regex_test "hack"
+RESULT=$?
+git stash pop -q 
+[ $RESULT -ne 0 ] && exit 1
+exit ${status}
